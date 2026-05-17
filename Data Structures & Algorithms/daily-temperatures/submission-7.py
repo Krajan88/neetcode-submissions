7a5c205d17a6d08349a90b16e2a3e7187e051f71
Krajan88 <krajan11011@gmail.com>
2026-05-17 13:56:28 -0400

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #stores days / indexes of temperatures
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            #if stack is empty OR top of the stack is greater than current temp
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                top = stack.pop()
+                res[top] = i - top
+                #add logic for actually computing the days
+            stack.append(i)
+               
+                
+
+        return res
