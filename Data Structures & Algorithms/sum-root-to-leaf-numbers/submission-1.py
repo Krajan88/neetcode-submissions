a87f6a38ef8566d35539f7b15571ed3d40655be6
Krajan88 <krajan11011@gmail.com>
2026-08-31 17:02:37 -0400

    Add: sum-root-to-leaf-numbers - submission-1

---
+++ Data Structures & Algorithms/sum-root-to-leaf-numbers/submission-1.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        #just keep track of the total number in the path and if both of the nodes left and right
+        #are nones append it to the list (that's the end of the path / end of the number)
+        self.nums = []
+        res = 0
+
+        def dfs(root, path):
+            if root is None:
+                return
+
+            path = path*10 + root.val
+
+            left, right = root.left, root.right
+
+            if left is None and right is None:
+                self.nums.append(path)
+
+
+            dfs(left, path)
+            dfs(right, path)
+
+            return
+
+        dfs(root, 0)
+        
+        for num in self.nums:
+            res += num
+
+        return res
