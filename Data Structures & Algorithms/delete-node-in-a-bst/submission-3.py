91bec77e836174255642a408c8b226d3c6099fd9
Krajan88 <krajan11011@gmail.com>
2026-09-02 14:51:16 -0400

    Add: delete-node-in-a-bst - submission-3

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-3.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+Solution 2: Find the node you want to delete and in its place return its right child. That way, the parent
+will point to the right child of the node want to delete. But first, find the min value in the right subtree
+of deletion node, and append the root.left to the left side of that node. 
+(basically reattach the del.left to min value of right subtree of root node)
+
+"""
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        if key > root.val:
+            #We assign root.right and root.left here beacuse here, the root represents the parent
+            #node of the node we want to delete. And that node that has to be deleted can be either
+            #left or right of the parent, so we have to change the .left/.right pointer
+            #of said parent
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+
+        else:
+            #here we are at the deleted node
+            if root.left is None:
+                return root.right
+
+            elif root.right is None:
+                return root.left
+                
+            else:
+                cur = root.right
+
+                while cur.left:
+                    cur = cur.left
+                
+                cur.left = root.left
+
+                
+                return root.right
+                
+
+        return root
+        
